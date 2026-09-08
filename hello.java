349aeda92f95f579de4820c206144f1b6eb3fda2
KingdomCross <firefox2025@duck.com>
2026-09-08 16:31:50 -0700

    init

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class BrainFuck
 {
     // Driver code
-    public static void main(String args[])
+    public static void main(String[] args)
     {
         System.out.println("Enter the code:");
         System.out.println("Output:");
