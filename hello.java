a98074c8c41282fddf3161bec6fc4db4f293547c
KingdomCross <firefox2025@duck.com>
2026-09-10 00:57:00 -0700

    scanner

--- hello.java
+++ hello.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 class BrainFuck
 {
+    private static Scanner ob = new Scanner(System.in);
+    private static int ptr; // Data pointer
+
     // Driver code
     public static void main(String[] args)
     {
