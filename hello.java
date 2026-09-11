2c3fa5790ad825be18549c0eb8c5cc0665fc19ed
KingdomCross <firefox2025@duck.com>
2026-09-11 04:19:56 -0700

    cleaning code

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
-// As I understand from the syllabus, we will be instructed when we will use BNF grammar.
+// As I understand from the syllabus, we will be instructed when we will use BNF grammar
 // This assignment we get to choose any language so it will be written in Java
-// Source from geeksforgeeks
+// Code source from geeksforgeeks.org "BrainFuck Interpreter in Java"
 // https://www.geeksforgeeks.org/java/brainfuck-interpreter-java/
 
 import java.util.Scanner;
@@ -11,25 +11,24 @@ class BrainFuck
     private static int ptr; // Data pointer
 
     // Max memory limit. Unsigned 8-bit binary
+    // "Maximum Number for a Byte: The 8-Bit Limit Explained [2024]"
     // https://thinkmatrix.blog/maximum-number-for-a-byte
     private static final int length = 255;
 
     // 256 bits from 0 to 255.
     private static final byte[] memory = new byte[length];
 
-    // Interpreter function which accepts the code a string parameter
     private static void interpret(String s)
     {
         int c = 0;
 
-        // Parsing through each character of the code
         for (int i = 0; i < s.length(); i++)
         {
             // > moves the pointer to the right
             if (s.charAt(i) == '>')
             {
-                if (ptr == length - 1)//If memory is full
-                    ptr = 0;//pointer is returned to zero
+                if (ptr == length - 1) // If memory is full
+                    ptr = 0; // pointer is returned to zero
                 else
                     ptr ++;
             }
@@ -38,9 +37,7 @@ class BrainFuck
             else if (s.charAt(i) == '<')
             {
                 if (ptr == 0) // If the pointer reaches zero
-
-                    // pointer is returned to rightmost memory position
-                    ptr = length - 1;
+                    ptr = length - 1; // pointer is returned to rightmost memory position
                 else
                     ptr --;
             }
@@ -49,19 +46,19 @@ class BrainFuck
             else if (s.charAt(i) == '+')
                 memory[ptr] ++;
 
-                // - decrements the value of the memory cell under the pointer
+            // - decrements the value of the memory cell under the pointer
             else if (s.charAt(i) == '-')
                 memory[ptr] --;
 
-                // . outputs the character signified by the cell at the pointer
+            // . outputs the character signified by the cell at the pointer
             else if (s.charAt(i) == '.')
                 System.out.print((char)(memory[ptr]));
 
-                // , inputs a character and store it in the cell at the pointer
+            // , inputs a character and store it in the cell at the pointer
             else if (s.charAt(i) == ',')
                 memory[ptr] = (byte)(ob.next().charAt(0));
 
-                // [ jumps past the matching ] if the cell under the pointer is 0
+            // [ jumps past the matching ] if the cell under the pointer is 0
             else if (s.charAt(i) == '[')
             {
                 if (memory[ptr] == 0)
@@ -97,12 +94,11 @@ class BrainFuck
         }
     }
 
-    // Driver code
-    public static void main(String[] args)
+    static void main()
     {
-        System.out.println("Enter the code:");
+        System.out.print("Enter the code: ");
         String code = ob.nextLine();
-        System.out.println("Output:");
+        System.out.print("Output: ");
         interpret(code);
     }
 }
